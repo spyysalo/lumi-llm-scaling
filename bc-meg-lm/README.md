bc2648b48c70149d35aee630790a32895295ad24
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2023-06-02 20:23:23 +0300

    Initial import

---
+++ bc-meg-lm/README.md
@@ -0,0 +1,97 @@
+# BigCode Megatron-LM on LUMI
+
+This directory contains scripts and documentation related to running the
+[BigCode fork of Megatron-LM](https://github.com/microsoft/Megatron-DeepSpeed)
+on LUMI.
+
+## Quickstart
+
+First create a new directory for your work (see top-level `README`) and
+then run the following in that directory:
+
+### Clone this repository
+
+```
+git clone https://github.com/spyysalo/lumi-llm-scaling.git
+```
+
+Work in this subdirectory
+
+```
+cd lumi-llm-scaling/bc-meg-lm
+```
+
+### Setup
+
+Run setup script to create virtual environment. This takes about 30 minutes.
+
+Clone the fork of Megatron-LM
+
+```
+git clone https://github.com/mayank31398/BigCode-Megatron-LM.git
+cd BigCode-Megatron-LM
+git checkout ontocord
+cd ..
+```
+
+### Data
+
+Download data in JSONL format
+
+```
+wget https://a3s.fi/lumi-llm-scaling/wikipedia_20220301.en.train.jsonl
+wget https://a3s.fi/lumi-llm-scaling/wikipedia_20220301.en.valid.jsonl
+```
+
+Download tokenizer
+
+```
+wget https://huggingface.co/gpt2/resolve/main/vocab.json
+wget https://huggingface.co/gpt2/resolve/main/merges.txt
+```
+
+Convert data to Megatron binary format on compute node.
+This takes about 30 minutes.
+
+```
+../scripts/cpu-interactive.sh 
+
+module load cray-python
+source venv/bin/activate
+
+mkdir data
+for f in wikipedia_20220301.en.{train,valid}.jsonl; do
+    python BigCode-Megatron-LM/tools/preprocess_data.py \
+        --input $f \
+        --output data/$(basename $f .jsonl) \
+        --dataset-impl mmap \
+        --tokenizer-type GPT2BPETokenizer \
+        --vocab vocab.json \
+        --merge-file merges.txt \
+        --append-eod \
+        --workers 128 \
+	--chunk-size 25
+done
+
+exit
+```
+
+**TODO: move data to flash, read from there**
+
+### Schedule batch job
+
+```
+sbatch pretrain_33B_8_node.sh 
+```
+
+### Summarize throughput results
+
+```
+python3 throughput.py logs/latest.out 
+```
+
+This should print approximately
+
+```
+TODO
+```
---
+++ bc-meg-lm/setup-venv.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Set up virtual environment for Megatron-DeepSpeed pretrain_gpt.py.
+
+# This script creates the directories venv and apex. If either of
+# these exists, ask to delete.
+for p in venv apex; do
+    if [ -e "$p" ]; then
+	read -n 1 -r -p "$p exists. OK to remove? [y/n] "
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+            echo "Removing $p."
+	    rm -rf "$p"
+	else
+            echo "Exiting."
+            exit 1
+	fi
+    fi
+done
+
+# Load modules
+module load cray-python
+module load LUMI/22.08 partition/G rocm/5.2.3
+
+module use /pfs/lustrep2/projappl/project_462000125/samantao-public/mymodules
+module load aws-ofi-rccl/rocm-5.2.3
+
+# Create and activate venv
+python -m venv --system-site-packages venv
+source venv/bin/activate
+
+# Upgrade pip etc.
+python -m pip install --upgrade pip setuptools wheel
+
+# Install pip packages
+python -m pip install --upgrade torch==1.13.1+rocm5.2 --extra-index-url https://download.pytorch.org/whl/rocm5.2
+python -m pip install --upgrade numpy datasets evaluate accelerate scikit-learn nltk
+python -m pip install --upgrade git+https://github.com/huggingface/transformers
+python -m pip install --upgrade deepspeed==0.8.1
+python -m pip install --upgrade tensorboard
+
+# Install apex on a GPU node
+git clone https://github.com/ROCmSoftwarePlatform/apex/
+
+# Use specific working commit (no longer needed as of May 2023)
+#cd apex
+#git checkout 5de49cc90051adf094920675e1e21175de7bad1b
+#cd -
+
+mkdir -p logs
+cat <<EOF > install_apex.sh
+#!/bin/bash
+#SBATCH --account=project_462000273
+#SBATCH --cpus-per-task=20
+#SBATCH --partition=standard-g
+#SBATCH --gres=gpu:mi250:1
+#SBATCH --time=1:00:00
+#SBATCH --output=logs/install_apex.out
+#SBATCH --error=logs/install_apex.err
+
+module load cray-python
+module load LUMI/22.08 partition/G rocm/5.2.3
+
+module use /pfs/lustrep2/projappl/project_462000125/samantao-public/mymodules
+module load aws-ofi-rccl/rocm-5.2.3
+
+source venv/bin/activate
+
+cd apex
+python setup.py install --cpp_ext --cuda_ext
+EOF
+
+echo "Installing apex on a GPU node. This is likely to take around 30 min."
+time sbatch --wait install_apex.sh
