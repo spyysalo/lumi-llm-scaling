69efc3e33d276fe0e7479f7d3c9e01e4cad7d28c
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2023-06-06 23:29:29 +0300

    Ignore first line by default

--- meg-ds/throughput.py
+++ meg-ds/throughput.py
@@ -4,14 +4,25 @@ import sys
 import re
 
 from statistics import mean, median, stdev
+from argparse import ArgumentParser
+
+
+
+def argparser():
+    ap = ArgumentParser()
+    ap.add_argument('--include-first', action='store_true')
+    ap.add_argument('log', nargs='+')
+    return ap
 
 
 RE = re.compile(r' samples per second: (\S+) .*? TFLOPs: (\S+)')
 
 
 def main(argv):
+    args = argparser().parse_args(argv[1:])
+
     samples, flops = [], []
-    for fn in argv[1:]:
+    for fn in args.log:
         with open(fn) as f:
             for l in f:
                 m = RE.search(l)
@@ -19,10 +30,19 @@ def main(argv):
                     s, t = m.groups()
                     samples.append(float(s))
                     flops.append(float(t))
+
     if not samples:
         print('no throughput lines found')
         return
 
+    if not args.include_first:
+        samples = samples[1:]
+        flops = flops[1:]
+
+    if len(samples) < 1:
+        print('not enough throughput lines found')
+        return        
+        
     def print_stats(label, d):
         print(
             label,
